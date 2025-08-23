2b2ce62a69a8a4119731e090eaaa9ccfe39f3816
NimsW20 <nimshiwanniarachchi@gmail.com>
2025-08-23 16:07:54 +0530

    new db feature

---
+++ Backend/manage-transformers/src/test/java/com/transformer/manage_transformers/ManageTransformersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transformer.manage_transformers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageTransformersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
